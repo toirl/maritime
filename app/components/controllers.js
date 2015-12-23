71827e7f1e421589d4f39c37135529061e070bf8
Torsten Irländer <torsten.irlaender@googlemail.com>
2015-12-23 22:54:06 +0100

    Renamed Tag controller

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -1,5 +1,5 @@
 var controllers = angular.module('maritime.controllers', []);
 
-controllers.controller('TagListCtrl', ['$scope', 'Tags', function($scope, Tags) {
+controllers.controller('TagCtrl', ['$scope', 'Tags', function($scope, Tags) {
   $scope.tags = Tags.query();
 }]);
