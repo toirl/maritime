ff9c285e4520feaacb1769621bb20cfdf870d10f
Torsten Irländer <torsten.irlaender@googlemail.com>
2015-12-23 22:56:26 +0100

    Added simple form to add new tags

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -2,4 +2,8 @@ var controllers = angular.module('maritime.controllers', []);
 
 controllers.controller('TagCtrl', ['$scope', 'Tags', function($scope, Tags) {
   $scope.tags = Tags.query();
+
+  $scope.add = function(tag) {
+    $scope.tags.push(angular.copy(tag));
+  }
 }]);
