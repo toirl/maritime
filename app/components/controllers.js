097fa36834f03fbe945a00edbce492301e39b8a9
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-01-03 23:08:57 +0100

    Added function to add the duration of all times

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -10,4 +10,15 @@ controllers.controller('TagCtrl', ['$scope', 'Tags', function($scope, Tags) {
 
 controllers.controller('TimesCtrl', ['$scope', 'Times', function($scope, Times) {
   $scope.times = Times.query();
+
+  // Calculate the total time of all time entries. Please note that this
+  // function seems to be called per item in the $scope.times array, and is
+  // recalled if this array changes.
+  $scope.totalTime = function(times) {
+    totalTime = 0;
+    for (var time = 0, len = $scope.times.length; time < len; time++) {
+      totalTime += $scope.times[time].duration;
+    }
+    return totalTime;
+  }
 }]);
