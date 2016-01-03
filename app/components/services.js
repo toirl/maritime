14e657941f52b05992231cc7061b3844c2d93f3d
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-01-03 22:25:01 +0100

    Added id param to services. Added some comments on default methods of the service which reimplement the default resource methods

--- app/components/services.js
+++ app/components/services.js
@@ -2,7 +2,10 @@ var services = angular.module('maritime.services', ['ngResource']);
 
 services.factory('Tags', ['$resource',
     function($resource) {
-        return $resource('tags', {}, {
+        return $resource('tags/:id', {id:'@id'}, {
+            // Default query method, this method can be deleted as it
+            // implements the same behaviour like the default query method of
+            // the resource.
             query: {method:'GET', isArray:true}
         });
     }
@@ -10,7 +13,10 @@ services.factory('Tags', ['$resource',
 
 services.factory('Times', ['$resource',
     function($resource) {
-        return $resource('times', {}, {
+        return $resource('times/:id', {id:'@id'}, {
+            // Default query method, this method can be deleted as it
+            // implements the same behaviour like the default query method of
+            // the resource.
             query: {method:'GET', isArray:true}
         });
     }
