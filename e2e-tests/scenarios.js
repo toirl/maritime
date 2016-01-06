2b2439f05bc145e6c6ff9a7b081265c9d26ca38c
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-01-06 22:16:03 +0100

    Removed testcases

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,7 +2,7 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('Maritime', function() {
 
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
@@ -16,27 +16,5 @@ describe('my app', function() {
     beforeEach(function() {
       browser.get('index.html#/view1');
     });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
   });
 });
