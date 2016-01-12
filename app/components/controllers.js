eba7f29ea4dada434626721d5782e3f670f73445
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-01-12 22:39:04 +0100

    Implemented a a running timer. Therefor we created a seperate controller per timer to be able to better interact with the angular-timer -> Refactored code.

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -59,61 +59,80 @@ controllers.controller('TimesCtrl', ['$scope', '$rootScope', 'Times', function($
   }
 }]);
 
-controllers.controller('TimersCtrl', ['$scope', '$rootScope', 'Timers', 'Times', function($scope, $rootScope, Timers, Times) {
+controllers.controller('TimersCtrl', ['$scope', 'Timers', function($scope, Timers) {
 
-  // All timers
   $scope.timers = Timers.query();
 
   $scope.add = function() {
     $scope.timers.push(angular.copy({"tags": [], "time": {"duration": 0, "state": 0}}));
   }
 
-  var setTags = function(time, tags) {
-    // Copy the tags to currently attached to the timer. This is needed as
-    // simply using timer.tags would only be a reference which means if you
-    // remove a tag from a timer the tag will be removed from the times
-    // created by this timer too. Using JSON here is the shortest way.
-    var copytags = JSON.parse(JSON.stringify(tags));
-    time.tags = copytags;
-  }
+}]);
 
-  $scope.start = function(timer) {
-    for (var i = 0, len = $scope.timers.length; i < len; i++) {
-      if ($scope.timers[i].time.state == 1) {
-        $scope.timers[i].time.state = 2;
+controllers.controller('TimerCtrl', ['$scope', '$rootScope', 'Timers', 'Times', function($scope, $rootScope, Timers, Times) {
+
+    var setTags = function(time, tags) {
+      // Copy the tags to currently attached to the timer. This is needed as
+      // simply using timer.tags would only be a reference which means if you
+      // remove a tag from a timer the tag will be removed from the times
+      // created by this timer too. Using JSON here is the shortest way.
+      var copytags = JSON.parse(JSON.stringify(tags));
+      time.tags = copytags;
+    }
+
+    $scope.start = function() {
+        if ($scope.timer.time.state == 2) {
+            console.log("Timer resumed", $scope.timer.time.state);
+            $scope.timer.time.state = 1;
+            $scope.$broadcast("timer-resume");
+        } else {
+            console.log("Timer start", $scope.timer.time.state);
+            $scope.timer.time = {"duration": 0, "state": 1, "start_date": moment().format('YYYY-MM-DD')}
+            $scope.$broadcast("timer-start");
+        }
+    };
+
+    $scope.pause = function() {
+        console.log("Timer paused", $scope.timer.time.state);
+        $scope.timer.time.state = 2;
+        $scope.$broadcast("timer-stop");
+    };
+
+    $scope.stop = function() {
+        $scope.$broadcast("timer-stop");
+        $scope.timer.time.state = 0;
+        $scope.timer.time.description = $scope.timer.description;
+        // Copy the tags to currently attached to the timer. This is needed as
+        // simply using timer.tags would only be a reference which means if you
+        // remove a tag from a timer the tag will be removed from the times
+        // created by this timer too. Using JSON here is the shortest way.
+        $scope.timer.time.tags = JSON.parse(JSON.stringify($scope.timer.tags));
+        // TODO: Actually save the new time
+        // Save the stopped time on the server
+        //Times.save($scope.timer.time);
+        $rootScope.$broadcast('timer:stopped',$scope.timer.time);
+    };
+
+    $scope.$on("timer-stopped", function(event, data) {
+        $scope.timer.time.duration = data.seconds + data.minutes*60 + data.hours*60*60;
+        console.log("Timer stopped", $scope.timer.time);
+    });
+
+    // Handling Dragging and dropping Tags.
+    $scope.DropCallback=function(event, ui, timer){
+      // FIXME: Using the rootscope like a global variable is hackish. Find a
+      // better way to find out which tag has been dragged (ti) <2016-01-05 00:54>
+      var tag = $rootScope.dragged;
+      if (timer.tags.indexOf(tag) == -1) {
+        timer.tags.push(tag);
       }
     }
-    timer.time = {"duration": 0, "state": 1, "start_date": moment().format('YYYY-MM-DD')}
-  }
 
-  $scope.stop = function(timer) {
-    timer.time.state = 0;
-    // TODO: Actually save the new time
-    // Save the stopped time on the server
-    //Times.save(timer.time);
-    // Notify listeners and provide the time that has been stopped
-    setTags(timer.time, timer.tags)
-    timer.time.description = timer.description;
-    $rootScope.$broadcast('timer:stopped',timer.time);
-  }
-
-  $scope.pause = function(timer) {
-    timer.time.state = 2;
-  }
-
-  $scope.DropCallback=function(event, ui, timer){
-    // FIXME: Using the rootscope like a global variable is hackish. Find a
-    // better way to find out which tag has been dragged (ti) <2016-01-05 00:54>
-    var tag = $rootScope.dragged;
-    if (timer.tags.indexOf(tag) == -1) {
-      timer.tags.push(tag);
+    $scope.removeTag = function(timer, tag) {
+      var index = timer.tags.indexOf(tag)
+      if (index > -1) {
+         timer.tags.splice(index, 1)
+      }
     }
-  }
 
-  $scope.removeTag = function(timer, tag) {
-    var index = timer.tags.indexOf(tag)
-    if (index > -1) {
-       timer.tags.splice(index, 1)
-    }
-  }
 }]);
