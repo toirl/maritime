6736f38915bb32fd17306ded1e4dfa30c08fc696
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-03-21 23:03:11 +0100

    Use url from AppConfig

--- app/components/services.js
+++ app/components/services.js
@@ -1,19 +1,8 @@
 var services = angular.module('maritime.services', ['ngResource']);
 
-services.factory('Tags', ['$resource',
-    function($resource) {
-        return $resource('json_data/tags/:id', {id:'@id'}, {
-            // Default query method, this method can be deleted as it
-            // implements the same behaviour like the default query method of
-            // the resource.
-            query: {method:'GET'}
-        });
-    }
-]);
-
-services.factory('Times', ['$resource',
-    function($resource) {
-        return $resource('json_data/times/:id', {id:'@id'}, {
+services.factory('Tags', ['$resource', 'AppConfig',
+    function($resource, AppConfig) {
+        return $resource(AppConfig.url+'/tags/:id', {id:'@id'}, {
             // Default query method, this method can be deleted as it
             // implements the same behaviour like the default query method of
             // the resource.
@@ -22,9 +11,21 @@ services.factory('Times', ['$resource',
     }
 ]);
 
-services.factory('Timers', ['$resource',
-    function($resource) {
-        return $resource('json_data/timers/:id', {id:'@id'}, {
+services.factory('Times', ['$resource', 'AppConfig',
+    function($resource, AppConfig) {
+        return $resource(AppConfig.url+'/times/:id', {id:'@id'}, {
+            // Default query method, this method can be deleted as it
+            // implements the same behaviour like the default query method of
+            // the resource.
+            query: {method:'GET'},
+            update: {method:'PUT'},
+        });
+    }
+]);
+
+services.factory('Timers', ['$resource', 'AppConfig',
+    function($resource, AppConfig) {
+        return $resource(AppConfig.url+'/timers/:id', {id:'@id'}, {
             // Default query method, this method can be deleted as it
             // implements the same behaviour like the default query method of
             // the resource.
