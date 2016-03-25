4813a6c05f793d937742446dfd7369da42ac7d8a
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-03-25 10:48:05 +0100

    Changed layout from bootstrap to material

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('maritime', [
+  'ngMaterial',
+  'ngMessages',
   'ngRoute',
   'ngDragDrop',
   'timer',
@@ -9,6 +11,12 @@ var app = angular.module('maritime', [
   'maritime.services',
   'maritime.controllers'
 ]);
+
+app.config(function($mdThemingProvider, $mdIconProvider){
+      $mdThemingProvider.theme('default')
+          .primaryPalette('blue-grey')
+          .accentPalette('red');
+});
 
 app.constant('AppConfig', {
     url: 'http://localhost:8080',
