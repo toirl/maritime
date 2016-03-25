c995d2b3cced27a97e389195a7d36a78acf9303e
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-03-25 15:20:57 +0100

    Changed output of duration

--- app/components/filters.js
+++ app/components/filters.js
@@ -1,7 +1,14 @@
 var filters = angular.module('maritime.filters', [])
 
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
 filters.filter('duration', function() {
-    return function(seconds) {
-        return moment.duration(seconds, 'seconds').humanize();
+    return function(sec) {
+        var duration = moment.duration(sec, 'seconds');
+        return pad(duration.hours(), 2)+":"+pad(duration.minutes(), 2)+":"+pad(duration.seconds(), 2)
     };
 });
